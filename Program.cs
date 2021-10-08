b9b1a76184185573b5d13b28590532c64939765a
Jeffe Fawkes <hobofawkes@gmail.com>
2021-10-08 13:26:55 -0500

    Read zip file

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO.Compression;
 
 namespace ipa_file_info
 {
@@ -6,7 +7,27 @@ namespace ipa_file_info
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Init.");
+
+            var path = @"C:\tmp\test.ipa";
+
+            using (var archive = ZipFile.OpenRead(path)) {
+                foreach (var entry in archive.Entries) {
+                    // Console.WriteLine($"Entry: {entry}");
+                    if (entry.FullName.EndsWith("Info.plist", StringComparison.Ordinal)) {
+                        Console.WriteLine($"Found Info.plist at {entry.FullName}");
+                        // TODO: read /only/ the Payload/Whatever.app/Info.plist file
+                        // Keys to read:
+                        // UISupportedDevices (Array of strings); devices allowed
+                        // DTSDKName (string); sdk built with?
+                        // UIRequiredDeviceCapabilities (array of strings); reqd architecture (e.g. armv7)
+                        // CFBundleName (string); app name
+                        // CFBundleShortVersionString (string); string in settings(?)
+                        // MinimumOSVersion (string); minimum iOS version (e.g. 9.3)
+                        // CFBundleIdentifier (string); bundle ID
+                    }
+                }
+            }
         }
     }
 }
