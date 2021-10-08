251627029a1dafff09ec45f66f3cb64b57bb3171
Jeffe Fawkes <hobofawkes@gmail.com>
2021-10-08 13:06:34 -0500

    Initial commit with stubs

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ipa_file_info
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
