48119e4766cf5bc1fd07eb9aa8c33b04eb4e4771
Jeffe Fawkes <hobofawkes@gmail.com>
2021-10-08 13:47:40 -0500

    Use Regex to get info.plist exclusively.

--- Program.cs
+++ Program.cs
@@ -1,10 +1,15 @@
-﻿using System;
+using System;
 using System.IO.Compression;
+using System.Linq;
+using System.Text.RegularExpressions;
 
 namespace ipa_file_info
 {
     class Program
     {
+        private static string InfoPlistRegex = @"^Payload\/.*\.app\/Info.plist$";
+        private static Regex PathMatch = new Regex(InfoPlistRegex);
+
         static void Main(string[] args)
         {
             Console.WriteLine("Init.");
@@ -12,21 +17,25 @@ namespace ipa_file_info
             var path = @"C:\tmp\test.ipa";
 
             using (var archive = ZipFile.OpenRead(path)) {
-                foreach (var entry in archive.Entries) {
-                    // Console.WriteLine($"Entry: {entry}");
-                    if (entry.FullName.EndsWith("Info.plist", StringComparison.Ordinal)) {
-                        Console.WriteLine($"Found Info.plist at {entry.FullName}");
-                        // TODO: read /only/ the Payload/Whatever.app/Info.plist file
-                        // Keys to read:
-                        // UISupportedDevices (Array of strings); devices allowed
-                        // DTSDKName (string); sdk built with?
-                        // UIRequiredDeviceCapabilities (array of strings); reqd architecture (e.g. armv7)
-                        // CFBundleName (string); app name
-                        // CFBundleShortVersionString (string); string in settings(?)
-                        // MinimumOSVersion (string); minimum iOS version (e.g. 9.3)
-                        // CFBundleIdentifier (string); bundle ID
-                    }
-                }
+                // var plists = archive.Entries.Where(entry => entry.Name.Equals("Info.plist"));
+                var infoPlist = archive.Entries.Where(entry => PathMatch.Match(entry.FullName).Success).FirstOrDefault();
+
+                // foreach (var entry in archive.Entries) {
+                //     // Console.WriteLine($"Entry: {entry}");
+                //     if (entry.FullName.EndsWith("Info.plist", StringComparison.Ordinal)) {
+                //         Console.WriteLine($"Found Info.plist at {entry.FullName}");
+                //         // TODO: read /only/ the Payload/Whatever.app/Info.plist file
+                //         // Keys to read:
+                //         // UISupportedDevices (Array of strings); devices allowed
+                //         // DTSDKName (string); sdk built with?
+                //         // UIRequiredDeviceCapabilities (array of strings); reqd architecture (e.g. armv7)
+                //         // CFBundleName (string); app name
+                //         // CFBundleShortVersionString (string); string in settings(?)
+                //         // MinimumOSVersion (string); minimum iOS version (e.g. 9.3)
+                //         // CFBundleIdentifier (string); bundle ID
+                //     }
+                // }
+                Console.WriteLine("Done.");
             }
         }
     }
