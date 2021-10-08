fdda224568a5be205a51b494eb1c977f2e0842be
Jeffe Fawkes <hobofawkes@gmail.com>
2021-10-08 14:21:40 -0500

    Use PList lib to actually read info.

--- Program.cs
+++ Program.cs
@@ -2,6 +2,7 @@ using System;
 using System.IO.Compression;
 using System.Linq;
 using System.Text.RegularExpressions;
+using Claunia.PropertyList;
 
 namespace ipa_file_info
 {
@@ -10,31 +11,47 @@ namespace ipa_file_info
         private static string InfoPlistRegex = @"^Payload\/.*\.app\/Info.plist$";
         private static Regex PathMatch = new Regex(InfoPlistRegex);
 
+        // static NSObject getVal(this NSDictionary dict, string key) {
+        //     return dict.ObjectForKey
+        // }
+
         static void Main(string[] args)
         {
             Console.WriteLine("Init.");
 
-            var path = @"C:\tmp\test.ipa";
+            var path = args.Length > 0 ? args[0] : @"C:\tmp\test.ipa";
+
+            Console.WriteLine("Name\t\tID\t\t\tMin iOS\tSDK\t\tApp Version");
 
             using (var archive = ZipFile.OpenRead(path)) {
-                // var plists = archive.Entries.Where(entry => entry.Name.Equals("Info.plist"));
                 var infoPlist = archive.Entries.Where(entry => PathMatch.Match(entry.FullName).Success).FirstOrDefault();
 
-                // foreach (var entry in archive.Entries) {
-                //     // Console.WriteLine($"Entry: {entry}");
-                //     if (entry.FullName.EndsWith("Info.plist", StringComparison.Ordinal)) {
-                //         Console.WriteLine($"Found Info.plist at {entry.FullName}");
-                //         // TODO: read /only/ the Payload/Whatever.app/Info.plist file
-                //         // Keys to read:
-                //         // UISupportedDevices (Array of strings); devices allowed
-                //         // DTSDKName (string); sdk built with?
-                //         // UIRequiredDeviceCapabilities (array of strings); reqd architecture (e.g. armv7)
-                //         // CFBundleName (string); app name
-                //         // CFBundleShortVersionString (string); string in settings(?)
-                //         // MinimumOSVersion (string); minimum iOS version (e.g. 9.3)
-                //         // CFBundleIdentifier (string); bundle ID
-                //     }
-                // }
+                // Now open the stream. Have to cast to dict.
+                var plist = (NSDictionary)PropertyListParser.Parse(infoPlist.Open());
+
+                // Keys to read:
+                // CFBundleName (string); app name
+                // CFBundleIdentifier (string); bundle ID
+                // DTSDKName (string); sdk built with?
+                // MinimumOSVersion (string); minimum iOS version (e.g. 9.3)
+                var appName    = plist.ObjectForKey("CFBundleName")?.ToString();
+                var bundleId   = plist.ObjectForKey("CFBundleIdentifier")?.ToString();
+                var sdkVersion = plist.ObjectForKey("DTSDKName")?.ToString();
+                var minIos     = plist.ObjectForKey("MinimumOSVersion")?.ToString();
+
+                // Below don't seem to be in all of 'em?
+                // CFBundleShortVersionString (string); string in settings(?)
+                // UISupportedDevices (Array of strings); devices allowed
+                // UIRequiredDeviceCapabilities (array of strings); reqd architecture (e.g. armv7)
+                var version    = plist.ObjectForKey("CFBundleShortVersionString")?.ToString();
+                var supportedDevices = ((NSArray)plist.ObjectForKey("UISupportedDevices"))?.GetArray();
+                var requiredDeviceCapabilities = ((NSArray)plist.ObjectForKey("UIRequiredDeviceCapabilities"))?.GetArray();
+
+                // Print 'em
+                Console.WriteLine($"{appName}\t{bundleId}\t{minIos}\t{sdkVersion}\t{version}");
+
+                // Close.
+
                 Console.WriteLine("Done.");
             }
         }
